c1cff74779bc0785fb37df82947dce08b653d743
damjan <damjan@archless.localdomain>
2009-09-04 18:29:07 +0200

    First erlang TCP client
    from http://www.erlang.org/doc/man/gen_tcp.html

---
+++ src/main.erl
@@ -0,0 +1,7 @@
+client() ->
+    SomeHostInNet = "localhost" % to make it runnable on one machine
+    {ok, Sock} = gen_tcp:connect(SomeHostInNet, 5678, 
+                                 [binary, {packet, 0}]),
+    ok = gen_tcp:send(Sock, "Some Data"),
+    ok = gen_tcp:close(Sock).
+
