5f273e24bd5c029e57fd159e5d59784668803a35
Damjan Georgievski <gdamjan@gmail.com>
2009-09-06 16:01:14 +0200

    First working client tcp socket
    Use:
    $ erl
    1> c(main).
    {ok,main}
    2> main:client().
    ok

--- src/main.erl
+++ src/main.erl
@@ -1,5 +1,10 @@
+-module(main).
+
+-export([client/0]).
+
+
 client() ->
-    SomeHostInNet = "localhost" % to make it runnable on one machine
+    SomeHostInNet = "localhost", % to make it runnable on one machine
     {ok, Sock} = gen_tcp:connect(SomeHostInNet, 5678, 
                                  [binary, {packet, 0}]),
     ok = gen_tcp:send(Sock, "Some Data"),
