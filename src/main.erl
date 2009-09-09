1ab306ab444b8a194950eb8a2940b5cf1acd79ae
Damjan Georgievski <gdamjan@gmail.com>
2009-09-09 02:37:07 +0200

    First successfull connection to a real IRC server
    Use:
    $ erl
    1> c(main).
    {ok,main}
    2> Pid = main:start(["irc.freenode.net", 6667, "erlbot_test", ["#lugola"]]).
    <0.91.0>
    3> Pid ! {self(), send_data, "PRIVMSG #lugola :success\r\n"}.

--- src/main.erl
+++ src/main.erl
@@ -1,12 +1,57 @@
 -module(main).
 
--export([client/0]).
+-export([start/1, client/4]).
 
+start(Args) ->
+    spawn(?MODULE, client, Args).
 
-client() ->
-    SomeHostInNet = "localhost", % to make it runnable on one machine
-    {ok, Sock} = gen_tcp:connect(SomeHostInNet, 5678, 
-                                 [binary, {packet, 0}]),
-    ok = gen_tcp:send(Sock, "Some Data"),
-    ok = gen_tcp:close(Sock).
+client(SomeHostInNet, Port, Nick, Channels) ->
+    {ok, Sock} = gen_tcp:connect(SomeHostInNet, Port,
+                    [binary, {active, true}, {packet, 0}]),
+    % must fire these very soon after connecting
+    gen_tcp:send(Sock, string:join(["NICK", Nick, "\r\n"], " ")), 
+    gen_tcp:send(Sock, string:join(["USER", Nick, "0 *  :", Nick, "\r\n"], " ")),
+    joinChannels(Sock, Channels),
+    main_loop(Sock).
 
+joinChannels(Sock, Channels) ->
+    [ Channel| Rest ] = Channels,
+    joinChannels(Sock, Channel, Rest).
+
+joinChannels(Sock, Channel, []) ->
+    gen_tcp:send(Sock, string:join(["JOIN", Channel, "\r\n"], " "));
+
+joinChannels(Sock, Channel, Channels) ->
+    joinChannels(Sock, Channel, []),
+    [ Channel_| Rest ] = Channels,
+    joinChannels(Sock, Channel_, Rest).
+
+main_loop(Sock) ->
+    receive
+        {Client, send_data, Binary} ->
+            case gen_tcp:send(Sock, [Binary]) of
+                ok ->
+                    Client ! {self(), data_sent},
+                    main_loop(Sock)
+            end;
+        {tcp, Sock, Data} ->
+            case process(Data) of
+                ok ->
+                    ok;
+                Answer ->
+                    gen_tcp:send(Sock, Answer)
+            end,
+            main_loop(Sock);
+        {tcp_closed, Sock} ->
+            io:format("Socket ~w closed [~w]~n", [Sock, self()]),
+            ok;
+        Other ->
+            io:format("Got ~w - goodbye!~n", [Other]),
+            gen_tcp:send(Sock, "QUIT : erlang sucks - just kidding :)\r\n"),
+            gen_tcp:close(Sock),
+            ok
+    end.
+
+process(Data) ->
+    io:format("~ts", [Data]),
+    ok.
