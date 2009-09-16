c42f5afb5d70b1bf07bf01c88719570adff9c472
Damjan Georgievski <gdamjan@gmail.com>
2009-09-16 19:49:12 +0200

    Implement CTCP-VERSION and CTCP-PING
    this was easy :)

--- src/handlers.erl
+++ src/handlers.erl
@@ -7,6 +7,10 @@ process(Line) ->
     case irc:parse(Line) of
         {match, [<<>>,<<>>,<<"PING">>, Server]} ->
             {respond, [<<"PONG :">>, Server]};
+        {match, [Sender, _User, <<"PRIVMSG">>, _Nick, <<"\^AVERSION\^A">>]} ->
+            {respond, ["NOTICE ", Sender, " :\^AVERSION erlbot-0.1\^A"]}; 
+        {match, [Sender, _User, <<"PRIVMSG">>, _Nick, <<"\^APING ", Rest/binary>>]} ->
+            {respond, ["NOTICE ", Sender, " :\^APING ", Rest]};
         _ ->
             ok
     end.
