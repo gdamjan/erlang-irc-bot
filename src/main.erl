7970f41c0c5c30a2f49e5a987368bc28113c563d
Damjan Georgievski <gdamjan@gmail.com>
2009-09-17 04:57:35 +0200

    Do the reconnection logic much simpler and straightforward
    It was stupid to try to reconnect from the main loop. Instead, now the main
    loop just falls off with a 'restart' atom and the connect function will
    reconnect if needed.

--- src/main.erl
+++ src/main.erl
@@ -2,24 +2,33 @@
 -author('gdamjan@gmail.com').
 -include_lib("common.hrl").
 
--export([start/1, client/4, codeswitch/2]).
+%% user interface
+-export([start/1]).
+
+%% exports for use within module only (used in apply, spawn)
+-export([client/4, codeswitch/1]).
 
 
 start(Args) ->
     spawn(?MODULE, client, Args).
 
 client(SomeHostInNet, Port, Nick, Channels) ->
-    client(SomeHostInNet, Port, Nick, Channels, 0).
+    connect(SomeHostInNet, Port, Nick, Channels, 1).
 
-client(SomeHostInNet, Port, Nick, Channels, Backoff) ->
-    % open a TCP connectin to the IRC server, we set the socket options to
+connect(SomeHostInNet, Port, Nick, Channels, Backoff) ->
+    % open a TCP connection to the IRC server, we set the socket options to
     % {packet, line} which means will receive data line-by-line (which is very
     % neat for the IRC protocol).
     % FIXME: error handling
-    {Sleep, NextBackoff} = utils:backoff(Backoff),
-    timer:sleep(Sleep),
     {ok, Sock} = login(SomeHostInNet, Port, Nick, Channels),
-    main_loop(Sock, [SomeHostInNet, Port, Nick, Channels, NextBackoff]).
+    case main_loop(Sock) of
+        restart ->
+            {Sleep, NextBackoff} = utils:backoff(Backoff),
+            timer:sleep(Sleep),
+            connect(SomeHostInNet, Port, Nick, Channels, NextBackoff);
+        _ ->
+            ok
+    end.
 
 login(SomeHostInNet, Port, Nick, Channels) ->
     {ok, Sock} = gen_tcp:connect(SomeHostInNet, Port,
@@ -57,27 +66,27 @@ joinChannels(Sock, Channel, Channels) ->
 % this is the main loop of the process, it will receive data from the socket
 % and also messages from other processes, will loop forever until an unknown
 % message is received.
-main_loop(Sock, Args) ->
+main_loop(Sock) ->
     receive
         % When the process receives this message, it will call 'codeswitch/1'
         % from the *latest* MODULE version,
         % codeswitch/1 just calls main_loop/1 again
         code_switch ->
             ?MODULE:codeswitch(Sock);
-        shut_down ->
+        quit ->
             io:format("Shuting down. "),
             quit(Sock),
             io:format("Bye.~n"),
             ok;
         restart ->
             quit(Sock),
-            apply(?MODULE, client, Args);
+            restart;
         % message received from another process
-        {Client, send_data, Binary} ->
-            case gen_tcp:send(Sock, [Binary]) of
+        {Client, send_data, Data} ->
+            case gen_tcp:send(Sock, [Data, ?CRNL]) of
                 ok ->
                     Client ! {self(), data_sent},
-                    main_loop(Sock, Args)
+                    main_loop(Sock)
             end;
         % data received from the socket
         {tcp, Sock, Data} ->
@@ -90,21 +99,21 @@ main_loop(Sock, Args) ->
                 _ ->
                     ok
             end,
-            main_loop(Sock, Args);
+            main_loop(Sock);
         % FIXME: handle errors on the socket
         {tcp_error, Sock, Reason} ->
             io:format("Socket ~w error: ~w [~w]~n", [Sock, Reason, self()]),
-            apply(?MODULE, client, Args);  % restart
+            restart;
         {tcp_closed, Sock} ->
             io:format("Socket ~w closed [~w]~n", [Sock, self()]),
-            apply(?MODULE, client, Args);  % restart
+            restart;
         % catch all, log and loop back
         CatchAll ->
             io:format("UNK: ~w~n", [CatchAll]),
-            main_loop(Sock, Args)
+            main_loop(Sock)
     end.
 
 % when this function is called Erlang will have the chance to run a new
 % main_loop(Sock) implementation (see: Hot code reloading)
-codeswitch(Sock, Args) ->
-    main_loop(Sock, Args).
+codeswitch(Sock) ->
+    main_loop(Sock).
