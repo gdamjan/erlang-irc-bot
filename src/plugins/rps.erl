6ee678497885eef81e999452cf0a8f0678ba5b09
Damjan Georgievski <gdamjan@gmail.com>
2009-12-08 16:34:49 +0100

    A rock-paper-scissors playing plugin.
    To load:
    c("plugins/rps.erl").
    gen_event:add_handler(plugins, 'plugins.rps', []).

---
+++ src/plugins/rps.erl
@@ -0,0 +1,59 @@
+-module(plugins.rps).
+-behaviour(gen_event).
+
+-author("gdamjan@gmail.com").
+-import(lists).
+-import(random).
+-export([init/1, handle_event/2, terminate/2]).
+
+
+init(_Args) ->
+    {ok, []}.
+
+handle_event(Msg, State) ->
+    case Msg of
+       {Pid, {match, [Nick, _Name, <<"PRIVMSG">>, Channel, <<"!rock">>]}} ->
+            Pid ! {send_data, ["PRIVMSG ", Channel, " :", Nick, play(rock)]},
+            {ok, State};
+       {Pid, {match, [Nick, _Name, <<"PRIVMSG">>, Channel, <<"!paper">>]}} ->
+            Pid ! {send_data, ["PRIVMSG ", Channel, " :", Nick, play(paper)]},
+            {ok, State};
+       {Pid, {match, [Nick, _Name, <<"PRIVMSG">>, Channel, <<"!scissors">>]}} ->
+            Pid ! {send_data, ["PRIVMSG ", Channel, " :", Nick, play(scissors)]},
+            {ok, State};
+        _ ->
+            {ok, State}
+    end.
+
+terminate(_Args, _State) ->
+    ok.
+
+
+%% Play a game of rock-paper-scissors
+play(PlayerAttack) ->  
+    ComputerAttack = get_computer_attack(),
+    case get_result(PlayerAttack, ComputerAttack) of
+        win ->
+            [": I chose ", atom_to_list(ComputerAttack), ". You win!"];
+        draw ->
+            [": I chose ", atom_to_list(ComputerAttack), ". It's a draw."];
+        lose ->
+            [": I chose ", atom_to_list(ComputerAttack), ". I WIN!"]
+    end.
+
+%% choose a computer attack at random 
+get_computer_attack() ->  
+    %% Get an index position at random 
+    Index = random:uniform(3), 
+    %% Pull out an attack
+    lists:nth(Index, [rock, paper, scissors]). 
+
+%% Determine the result of an attack
+get_result(Player1, Player2) -> 
+    case {Player1, Player2} of 
+        {rock, scissors} -> win; 
+        {paper, rock} -> win; 
+        {scissors, paper} -> win; 
+        {Same, Same} -> draw; 
+        {_,_} -> lose 
+    end.
