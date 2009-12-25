8e3e856bd9fcace83475ab1764ec714b5255bb8c
Damjan Georgievski <gdamjan@gmail.com>
2009-12-25 15:42:39 +0100

    Refactor the tell.erl plugin
    It's also less noisy on the channel now.
    Replies with private NOTICE messages.

--- src/plugins/tell.erl
+++ src/plugins/tell.erl
@@ -6,27 +6,44 @@
 
 -import(re).
 -import(dict).
+-import(lists).
 
 init(_Args) ->
     {ok, dict:new()}.
 
+
+fancy_time(T) ->
+    "{time}".
+
+remember(Pid, Channel, From, Msg, State) ->
+    Timestamp = 1,
+    [Recepient | Message] = re:split(Msg, " ", [{parts,2}]),
+    Pid ! {send_data, ["NOTICE ", From, " :ok, I'll  pass that to ", Recepient]},
+    {ok, dict:append(Recepient, {Timestamp, Channel, From, Message}, State)}.
+
+reminder(Pid, Nick, State) ->
+    case dict:is_key(Nick, State) of
+        true ->
+            L = dict:fetch(Nick, State),
+            lists:foreach(fun ({Timestamp, Channel, From, Message}) ->
+    		Msg =  [fancy_time(Timestamp), " ", From, " on ", Channel, ": ", Message],
+    		Pid ! {send_data, ["NOTICE ", Nick, " :", Msg]} end, L),
+            {ok, dict:erase(Nick, State)};
+        false ->
+            {ok, State}
+    end.
+
 handle_event(Msg, State) ->
     case Msg of
-        {Pid, {match, [Nick, _Name, <<"PRIVMSG">>, <<"#",Channel/binary>>, <<"!tell ",Tale/binary>>]}} ->
-            [Whom | Message] = re:split(Tale, " ", [{parts,2}]),
-            Pid ! {send_data, ["PRIVMSG ", <<"#",Channel/binary>>, " :ok"]},
-            {ok, dict:store({Whom, Channel}, {Nick, Message}, State)};
-        {Pid, {match, [Nick, _Name, <<"JOIN">>, <<"#",Channel/binary>>]}} ->
-            Key = {Nick, Channel},
-            case dict:is_key(Key, State) of
-                true ->
-                    {Whom, Message} = dict:fetch(Key, State),
-                    Pid ! {send_data, ["PRIVMSG ", <<"#",Channel/binary>>, " :", 
-                        "Hey ", Nick, ", ", Whom, " told me to tell you: ", Message]},
-                    {ok, dict:erase(Key, State)};
-                false ->
-                    {ok, State}
-            end;
+        {Pid, {match, [Sender, _Name, <<"PRIVMSG">>, <<"#",Channel/binary>>, <<"!tell ",Rest/binary>>]}} ->
+            remember(Pid, Channel, Sender, Rest, State);
+        {Pid, {match, [Sender, _Name, <<"PRIVMSG">>, <<"#",Channel/binary>>, <<"!ask ",Rest/binary>>]}} ->
+            remember(Pid, Channel, Sender, Rest, State);
+
+        {Pid, {match, [Sender, _Name, <<"JOIN">>, <<"#",_Channel/binary>>]}} ->
+            reminder(Pid, Sender, State);
+        {Pid, {match, [_Sender, _Name, <<"NICK">>, Nick]}} ->
+            reminder(Pid, Nick, State);
         _ ->
             {ok, State}
     end.
