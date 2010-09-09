615661ab2346aa12ba7c23acb25ec8a4b120f4e3
Damjan Georgievski <gdamjan@gmail.com>
2010-09-09 18:30:42 +0200

    Add a clever macro to convert proplist to record
    This is a fairly usefull generic macro that creates a function to convert from
    a proplist to an Erlang record (a tagged tuple). I'll use it in some plugins
    for config keeping.

--- src/ircbot.hrl
+++ src/ircbot.hrl
@@ -5,3 +5,16 @@
 -define(RECV_TIMEOUT, 180000). % 180sec
 -define(CONNECT_TIMEOUT, 60000). % 60sec
 -define(KEEPALIVE, 60000). % 1min
+
+
+%%% a macro that makes a function that converts from a property list to an
+%%% Erlang record (a taged tuple).
+%%% The function has the signature proplist_to_record(Record, Proplist)
+-define(make_proplist_to_record(Record),
+   proplist_to_record(Record, Proplist) ->
+       Fields = record_info(fields, Record),
+       [Tag| Values] = tuple_to_list(#Record{}),
+       Defaults = lists:zip([Tag|Fields], [Tag|Values]),
+       L = lists:map(fun ({K,V}) -> proplists:get_value(K, Proplist, V) end, Defaults),
+       list_to_tuple(L)
+).
