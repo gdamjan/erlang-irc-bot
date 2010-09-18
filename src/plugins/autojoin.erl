0c55ee7c7d23831a2e739466790fc845d1d73e70
Damjan Georgievski <gdamjan@gmail.com>
2010-09-18 03:01:15 +0200

    New plugin: autojoin channels when invited

---
+++ src/plugins/autojoin.erl
@@ -0,0 +1,24 @@
+-module(plugins.autojoin).
+-author("gdamjan@gmail.com").
+
+-behaviour(gen_event).
+-export([init/1, handle_event/2, terminate/2, handle_call/2, handle_info/2, code_change/3]).
+
+% responds to INVITEs by joining the channel
+
+init(_Args) ->
+    {ok, []}.
+
+handle_event(Msg, State) ->
+    case Msg of
+        {in, Ref, [_Sender, _User, <<"INVITE">>, _Nick, Channel]} ->
+            Ref:join(Channel);
+        _ ->
+            ok
+    end,
+    {ok, State}.
+
+handle_call(_Request, State) -> {ok, ok, State}.
+handle_info(_Info, State) -> {ok, State}.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+terminate(_Args, _State) -> ok.
