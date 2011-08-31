4447fa82517e0b66df170241233dca455a5fa183
Damjan Georgievski <gdamjan@gmail.com>
2011-08-31 14:49:38 +0200

    A bit of readability in the .hrl include file

--- src/ircbot.hrl
+++ src/ircbot.hrl
@@ -2,11 +2,14 @@
 -define(REALNAME, "An experimental Erlang IRC bot").
 -define(QUITMSG, "I can feel it, my mind is going...").
 
--define(SEND_TIMEOUT, 10000).    % 10sec
--define(RECV_TIMEOUT, 180000).   % 180sec
+-define(SECOND, 1000).
+-define(MINUTE, 60 * 1000).
 
--define(CONNECT_TIMEOUT, 5000).    % wait for dns, but no more. 10s
--define(REGISTER_TIMEOUT, 30000).  % 30s
--define(RECONNECT_DELAY, 15000).    % fast reconnect 15s
--define(BACKOFF_DELAY, 15000).      % backoff reconnect 15s base,
+-define(RECV_TIMEOUT,      3 * ?MINUTE).
+-define(SEND_TIMEOUT,     10 * ?SECOND).
+
+-define(CONNECT_TIMEOUT,   5 * ?SECOND).  % wait for dns
+-define(REGISTER_TIMEOUT, 30 * ?SECOND).  % wait for register on irc
+-define(RECONNECT_DELAY,  15 * ?SECOND).  % fast reconnect
+-define(BACKOFF_DELAY,    15 * ?SECOND).  % backoff reconnect 15s base,
                                    % delay of 0, 5, 20, 45, 80, 125
