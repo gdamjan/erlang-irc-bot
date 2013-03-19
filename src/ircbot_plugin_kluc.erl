e1e0b7e90341fb9af3b9463e9509f6b1dff3b10e
Damjan Georgievski <gdamjan@gmail.com>
2013-03-19 22:38:56 +0100

    повеќе промени во една линија

--- src/ircbot_plugin_kluc.erl
+++ src/ircbot_plugin_kluc.erl
@@ -15,14 +15,42 @@ handle_event(Msg, State) ->
             Text = sets:fold(fun (E, AccIn) -> <<AccIn/binary, " ", E/binary>> end, <<"Клучеви имаат:">>, State),
             Ref:privmsg(Channel, Text),
             {ok, State};
-        {in, _Ref, [_Sender, _Name, <<"PRIVMSG">>, _Channel, <<"!клуч +", Rest/binary>>]} ->
+        {in, Ref, [_Sender, _Name, <<"PRIVMSG">>, Channel, <<"!клуч +", Rest/binary>>]} ->
+            Ref:notice(Channel, <<Rest/binary, " додаден">>),
             {ok, sets:add_element(Rest, State)};
-        {in, _Ref, [_Sender, _Name, <<"PRIVMSG">>, _Channel, <<"!клуч -", Rest/binary>>]} ->
+        {in, Ref, [_Sender, _Name, <<"PRIVMSG">>, Channel, <<"!клуч -", Rest/binary>>]} ->
+            Ref:notice(Channel, <<Rest/binary, " отстранет">>),
             {ok, sets:del_element(Rest, State)};
-
+        {in, Ref, [_Sender, _Name, <<"PRIVMSG">>, Channel, <<"!клучеви ", Rest/binary>>]} ->
+            {NewSet, Response} = process_changes(Rest, State),
+            Ref:notice(Channel, Response),
+            {ok, NewSet};
         _ ->
             {ok, State}
     end.
+
+
+process_changes(Line, Set) ->
+    List = binary:split(Line, <<" ">>, [global, trim]),
+    process_list(List, Set, []).
+
+process_list([], Set, Text) ->
+    {Set, Text};
+
+process_list([Head|Tail], Set, Text) ->
+    case Head of
+        <<"+", Rest/binary>> ->
+            NewSet = sets:add_element(Rest, Set),
+            NewText = <<Rest/binary, " додаден ">>,
+            process_list(Tail, NewSet, [Text|NewText]);
+        <<"-", Rest/binary>> ->
+            NewSet = sets:del_element(Rest, Set),
+            NewText = <<Rest/binary, " отстранет ">>,
+            process_list(Tail, NewSet, [Text|NewText]);
+        _ ->
+            process_list(Tail, Set, Text)
+    end.
+
 
 handle_call(_Request, State) -> {ok, ok, State}.
 handle_info(_Info, State) -> {ok, State}.
