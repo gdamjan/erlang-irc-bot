6f4a070a96026d2d61e9e7438d010f90564989f5
Damjan Georgievski <gdamjan@gmail.com>
2014-12-05 20:16:44 +0100

    split the supervisor from the app

--- src/ircbot_app.erl
+++ src/ircbot_app.erl
@@ -2,16 +2,12 @@
 -author('gdamjan@gmail.com').
 
 -behaviour(application).
--behaviour(supervisor).
+-export([start/2, stop/1]).
 
-%% Application and Supervisor callbacks
--export([start/2, stop/1, init/1]).
+-define(SUPERVISOR, ircbot_sup).
 
 %% API
 -export([start/0]).
-
--define(SUPERVISOR, ?MODULE).
--define(CHILD(I), {I, {I, start_link, []}, permanent, 5000, worker, [I]}).
 
 start() ->
     application:start(sasl),
@@ -20,27 +16,19 @@ start() ->
 
 %% app behaviour
 start(_Type, _StartArgs) ->
-    {ok, Sup} = supervisor:start_link({local, ?SUPERVISOR}, ?MODULE, []),
+    {ok, Sup} = supervisor:start_link({local, ?SUPERVISOR}, ?SUPERVISOR, []),
     {ok, SettingsFile} = init:get_argument(conf),
     {ok, Settings} = file:consult(SettingsFile),
-    connect_all(Sup, Settings),
+    start_all(Sup, Settings),
     {ok, Sup}.
 
 stop(_State) ->
     exit(whereis(?SUPERVISOR), shutdown).
 
-
-%% supervisor behaviour
-init([]) ->
-    {ok, {{simple_one_for_one, 10, 60}, [?CHILD(ircbot_fsm)]}}.
-
-
-%% helper
-connect_all(Sup, Settings) ->
+start_all(Supervisor, Settings) ->
     lists:foreach(
         fun ({connection, Args}) ->
-                {ok, Child} = supervisor:start_child(Sup, [Args]),
-                gen_fsm:send_event(Child, connect)
+                {ok, _Child} = supervisor:start_child(Supervisor, [Args])
         end,
         Settings
     ).
---
+++ src/ircbot_sup.erl
@@ -0,0 +1,14 @@
+-module(ircbot_sup).
+-author('gdamjan@gmail.com').
+
+-behaviour(supervisor).
+-export([init/1]).
+
+-define(CHILD(I), {I, {I, start_link, []}, permanent, 5000, worker, [I]}).
+
+%% supervisor behaviour
+init([]) ->
+    {ok, {
+        {simple_one_for_one, 10, 60},
+        [?CHILD(ircbot_fsm)]
+    }}.
