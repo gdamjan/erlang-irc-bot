c85d67963207cdf083ca9cc602fbd27129fc1d06
Alexander Müssig <alexander.muessig@googlemail.com>
2016-08-26 16:33:50 +0200

    Added linebreaks in MAC OS X

--- src/ircbot_log.erl
+++ src/ircbot_log.erl
@@ -28,7 +28,7 @@ debug(out, Msg) ->
 % print directly to stdout thus avoid Erlangs broken
 % io:* routines
 debug(Msg) ->
-    port_command(stdout, [Msg, "\n"]).
+    port_command(stdout, [Msg, "\r\n"]).
 
 % open stdout as an Erlang port and register it with the
 % stdout atom. The port will be closed automatically if the
